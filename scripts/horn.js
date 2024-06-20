d24ee0b8827aca8f5700e92af89692ab462916ed
MxEggSalad <mxeggsalad@proton.me>
2024-06-20 20:45:32 +1000

    Revert "Dont mind this. homework."
    This reverts commit 0e408e738eb0852b61ecf5bd0a2511ebab05b70c.

--- scripts/horn.js
+++
@@ -1,6 +0,0 @@
-// selects an img from the page, changes it to gif for set ammount of time (i just did this one because i ran out of things to do ahahahah)
-function hornGif(){
-  document.getElementById("horn").src="/horn/assets/horn.gif";
-  document.getElementById("hornsound").play()
-  setTimeout(function(){ document.getElementById("horn").src="/horn/assets/skeleton.jpeg"; }, 2300);
-}
